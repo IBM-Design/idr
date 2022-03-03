473bca739820ed839c923bda3c0995e5ed0a1b36
sstrubberg <sstrubberg@protonmail.com>
2022-03-03 12:16:42 +0000

    Deploying to gh-pages from @ IBM-Design/idr@4ad175e0f51daed1800352ee983bf7c5d2c53da4 🚀

---
+++ design/research/component---src-pages-404-js-0f0335790a5395cc5ddd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkidr_website_gatsby_theme=self.webpackChunkidr_website_gatsby_theme||[]).push([[883],{9616:function(e,t,n){n.r(t);var s=n(7294),r=n(7426),o=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];t.default=function(){return s.createElement(r.e9,{links:o})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0f0335790a5395cc5ddd.js.map
