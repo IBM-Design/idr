da76ca5d3c640dece628fed8407333fc809a11d2
Scott Strubberg <sstrubberg@us.ibm.com>
2022-12-02 14:24:45 -0600

    chore(site): added common.js and custom footer (#177)
    * chore(site): added common.js and custom footer
    
    * fix(footer): added copy

---
+++ gatsby-ssr.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  if (process.env.NODE_ENV !== `production`) {
+    return null;
+  }
+
+  const script = `
+  if(!window) window = {};
+  window.idaPageIsSPA = true;
+  window.digitalData = {
+    page: {
+      category: {
+        primaryCategory: 'DESIGN',
+      },
+      pageInfo: {
+        ibm: {
+          siteID: 'DESIGN_RESEARCH',
+          country: 'US',
+          industry: 'Design',
+          owner: 'carbon@us.ibm.com',
+        },
+      },
+    },
+  }`;
+
+  return setPostBodyComponents([
+    <script key="digital-data" dangerouslySetInnerHTML={{ __html: script }} />,
+    <script
+      defer
+      key="core-metrics"
+      src="https://1.www.s81c.com/common/stats/ibm-common.js"
+      type="text/javascript"
+    />,
+  ]);
+};
